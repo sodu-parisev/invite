0d1dd5966f634f300a510fd98cbe1debcde52513
sodu.parsiev <sodiq915@gmail.com>
2023-03-09 00:52:11 +0500

    title

--- resources/views/layouts/invite.blade.php
+++ resources/views/layouts/invite.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ __("Invitation to the event") }}</title>
+        <title>{{ __("Teaching Course 17-18 марта 2023 г.") }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
